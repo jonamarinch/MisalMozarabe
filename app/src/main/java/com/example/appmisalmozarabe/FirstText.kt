8169ef7bc90c4252ed4d18e102f935cda039dcce
jonamarinch <jonamarinch@gmail.com>
2025-01-28 17:37:16 +0100

    Página de inicio creada

--- app/src/main/java/com/example/appmisalmozarabe/FirstText.kt
+++ app/src/main/java/com/example/appmisalmozarabe/FirstText.kt
@@ -6,14 +6,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
-import com.example.appmisalmozarabe.databinding.FragmentFirstBinding
+import com.example.appmisalmozarabe.databinding.TextFirstBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class FirstText : Fragment() {
 
-    private var _binding: FragmentFirstBinding? = null
+    private var _binding: TextFirstBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,7 +24,7 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentFirstBinding.inflate(inflater, container, false)
+        _binding = TextFirstBinding.inflate(inflater, container, false)
         return binding.root
 
     }
